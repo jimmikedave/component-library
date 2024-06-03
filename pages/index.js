5039a4b9b8b068332879d111e43a66bbbc818203
Jim David <jimmichael.david@gmail.com>
2024-06-02 20:44:17 -0400

    configure layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
     
     return (
-        <div>
-            
+        <div className='home'>
+            <div className='example'>bruh</div>
         </div>
     )
 }
